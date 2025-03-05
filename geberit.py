5d50cd7ed335de505913f58fd9d00e2ee1214bd6
pra78 <66877961+pra78@users.noreply.github.com>
2025-03-05 21:45:31 +0200

    styling

--- geberit.py
+++ geberit.py
@@ -27,31 +27,85 @@ app.layout = html.Div(
             ],
             className="header",
         ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["time"],
+                                    "y": data["15-2 / Humidity"],
+                                    "type": "lines",
+                                    "hovertemplate": (
+                                        "%{y:.2f}%<extra></extra>"
+                                    ),
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Humidity",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {
+                                    "tickprefix": "$",
+                                    "fixedrange": True,
+                                },
+                                "colorway": ["#17b897"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
 
+                # ...
 
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data['time'],
-                        "y": data["15-2 / Humidity"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Humidity"},
-            },
+            ],
+            className="wrapper",
         ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["time"],
-                        "y": data["15-2 / Temperature"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Temperature"},
-            },
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["time"],
+                                    "y": data["15-2 / Temperature"],
+                                    "type": "lines",
+                                    "hovertemplate": (
+                                        "%{y:.2f}°<extra></extra>"
+                                    ),
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Temperature",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {
+                                    "tickprefix": "$",
+                                    "fixedrange": True,
+                                },
+                                "colorway": ["#17b897"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+
+                # ...
+
+            ],
+            className="wrapper",
         ),
     ]
 )
